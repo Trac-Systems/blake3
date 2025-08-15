900a69025c41510334da3a4e685004507a499ed1
Jusuf Suljic <jusuf.uni@gmail.com>
2025-08-15 14:44:30 +0200

    Blake3 tests added

---
+++ test/blake3.test.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai')
+const { blake3 } = require('../src/index')
+
+const toHex = (uint8Arr) =>
+  Array.from(uint8Arr)
+    .map(b => b.toString(16).padStart(2, '0'))
+    .join('')
+
+describe('blake3 hashing', function () {
+  this.timeout(5000) 
+
+  const input = 'BLAKE3'
+  const lengthsToTest = [16, 32, 48, 64]
+
+  lengthsToTest.forEach((len) => {
+    it(`should return a hash of length ${len} bytes`, async () => {
+      const hash = await blake3(input, len)
+      expect(hash).to.be.instanceOf(Uint8Array)
+      expect(hash.length).to.equal(len)
+      console.log(`Length ${len}: ${toHex(hash)}`)
+    })
+  })
+
+  it('should produce the same output for the same input and length', async () => {
+    const hash1 = await blake3(input, 32)
+    const hash2 = await blake3(input, 32)
+    expect(toHex(hash1)).to.equal(toHex(hash2))
+  })
+
+  it('should produce different outputs for different lengths', async () => {
+    const hash32 = await blake3(input, 32)
+    const hash64 = await blake3(input, 64)
+    expect(toHex(hash32)).to.not.equal(toHex(hash64))
+  })
+})
