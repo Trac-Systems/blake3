659d1baf83eb25446956d0239ab581babc9330ef
leonardotc <ltc270486@hotmail.com>
2025-08-15 12:20:22 +0200

    Update makefile

--- src/index.js
+++ src/index.js
@@ -1,41 +1,29 @@
 const createBlake3 = require('./internal/blake3_wasm')
 
 const DEFAULT_LENGTH = 32
-
 let wasm
 
-const stringToBytes = str => {
-  const bytes = new Uint8Array(str.length);
-  for (let i = 0; i < str.length; i++) {
-    bytes[i] = str.charCodeAt(i)
-  }
-  return bytes
-}
-
 const getWasm = async () => {
-  // if (wasm) return wasm
+  if (wasm) return wasm
   wasm = await createBlake3()
   return wasm
 }
 
-module.exports.blake3 = async (input, hashLength = DEFAULT_LENGTH) => {
+module.exports.blake3 = async (bytes, hashLength = DEFAULT_LENGTH) => {
   const wasm = await getWasm()
-
-  const bytes = stringToBytes(input)
-  const ptr = wasm._malloc(bytes.length)
-
-  wasm.HEAPU8.set(bytes, ptr)
+  const inputAddr = wasm._malloc(bytes.length)
+  wasm.HEAPU8.set(bytes, inputAddr)
   const outputAddr = wasm._malloc(hashLength)
 
   wasm.ccall(
     'hash',
     null,
     ['number', 'number', 'number', 'number'],
-    [ptr, bytes.length, outputAddr, hashLength],
+    [inputAddr, bytes.length, outputAddr, hashLength],
   )
 
   const res = wasm.HEAPU8.subarray(outputAddr, outputAddr + hashLength)
   wasm._free(outputAddr)
-  wasm._free(bytes)
+  wasm._free(inputAddr)
   return res
 }
