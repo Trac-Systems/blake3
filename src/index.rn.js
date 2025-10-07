4b0a67803e80d979c6af40faf48095af6de45c98
leonardotc <ltc270486@hotmail.com>
2025-10-07 10:46:04 +0200

    Add react native support

---
+++ src/index.rn.js
@@ -0,0 +1,30 @@
+import createBlake3 from './internal/blake3_wasm_portable.js'
+
+const DEFAULT_LENGTH = 32
+let wasm
+
+const getWasm = async () => {
+  if (wasm) return wasm
+  wasm = await createBlake3()
+  return wasm
+}
+
+export const blake3 = async (bytes, hashLength = DEFAULT_LENGTH) => {
+  const wasm = await getWasm()
+  const inputAddr = wasm._malloc(bytes.length)
+
+  wasm.HEAPU8.set(bytes, inputAddr)
+  const outputAddr = wasm._malloc(hashLength)
+
+  wasm.ccall(
+    'hash',
+    null,
+    ['number', 'number', 'number', 'number'],
+    [inputAddr, bytes.length, outputAddr, hashLength],
+  )
+
+  const res = wasm.HEAPU8.subarray(outputAddr, outputAddr + hashLength)
+  wasm._free(outputAddr)
+  wasm._free(inputAddr)
+  return res
+}
